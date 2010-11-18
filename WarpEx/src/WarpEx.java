6e0d7caaa37f51af3584b2bdc2a6dfee36ee8a09
palm3r <palm3r.x@gmail.com>
2010-11-18 19:12:10 +0900

    * changed property key "default-ns" to "default-namespace" in WarpEx

--- WarpEx/src/WarpEx.java
+++ WarpEx/src/WarpEx.java
@@ -16,7 +16,7 @@ public class WarpEx extends PluginEx {
 	}
 
 	private static final String DATA_FILE = "data.txt";
-	private static final String DEFAULT_NAMESPACE_KEY = "default-ns";
+	private static final String DEFAULT_NAMESPACE_KEY = "default-namespace";
 
 	private Map<String, Location> warps = new HashMap<String, Location>();
 	private NS defaultNamespace = NS.Personal;
